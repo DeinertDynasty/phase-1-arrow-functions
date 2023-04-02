af6651dd36cbccc43ce8473bfdf7bd24273b27fd
Phoenix Deinert <phoenix.deinert@gmail.com>
2023-04-02 19:05:34 -0400

    lab complete

---
+++ index.js
@@ -0,0 +1,18 @@
+function divide(a, b) {
+    return a / b;
+  }
+  
+  const square = (x) => {
+    return x * x;
+  }
+  
+  const add = (a, b) => {
+    return a + b;
+  }
+  
+  module.exports = {
+    divide,
+    square,
+    add,
+  };
+  
